d4511243b05a16e9a8ab6d921d68b1795091946c
Mohammed Salem <mohammed.fuad@gmail.com>
2022-06-08 12:55:57 +0200

    Restructure folders
    * Moved packages to src folder
    * Moved unit test to tests folder
    * Change EF provider options to InMemory to fix failed unit test.
    
    Issue #2

---
+++ tests/ATag.Tests/DatabaseFixture.cs
@@ -0,0 +1,20 @@
+﻿namespace ATag.Tests
+{
+	using ATag.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore;
+
+	public class DatabaseFixture
+	{
+		private readonly DbContextOptions options;
+
+		public DatabaseFixture()
+		{
+			this.options = new DbContextOptionsBuilder().UseInMemoryDatabase("Db").Options;
+		}
+
+		public DataContext CreateDataContext()
+		{
+			return new DataContext(this.options);
+		}
+	}
+}
